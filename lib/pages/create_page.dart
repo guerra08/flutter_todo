d760b8452e29a63a8db25eb66014b020c78a3b03
Bruno Guerra <gguerrabruno@outlook.com>
2022-02-02 18:09:20 -0300

    Makes the create screen better

--- lib/pages/create_page.dart
+++ lib/pages/create_page.dart
@@ -15,6 +15,15 @@ class _CreatePageState extends State<CreatePage> {
   final _formKey = GlobalKey<FormState>();
   String? _title, _description;
 
+  Future<void> submitForm() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState?.save();
+      await locator.get<TaskService>().addNewTask(
+          Task(title: _title!, description: _description ?? "", isDone: false));
+      GoRouter.of(context).pop();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,34 +34,42 @@ class _CreatePageState extends State<CreatePage> {
         key: _formKey,
         child: Column(
           children: [
-            TextFormField(
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return "Please enter the title";
-                } else {
-                  return null;
-                }
-              },
-              onSaved: (value) {
-                _title = value;
-              },
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Task title",
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Please enter the title";
+                  } else {
+                    return null;
+                  }
+                },
+                textInputAction: TextInputAction.next,
+                onSaved: (value) {
+                  _title = value;
+                },
+              ),
             ),
-            TextFormField(
-              onSaved: (value) {
-                _description = value;
-              },
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  hintText: "Task description",
+                ),
+                textInputAction: TextInputAction.done,
+                onSaved: (value) {
+                  _description = value;
+                },
+                onFieldSubmitted: (_) {
+                  submitForm();
+                },
+              ),
             ),
             ElevatedButton(
-              onPressed: () async {
-                if (_formKey.currentState!.validate()) {
-                  _formKey.currentState?.save();
-                  await locator.get<TaskService>().addNewTask(Task(
-                      title: _title!,
-                      description: _description ?? "",
-                      isDone: false));
-                  GoRouter.of(context).pop();
-                }
-              },
+              onPressed: submitForm,
               child: const Text("Add"),
             ),
           ],
