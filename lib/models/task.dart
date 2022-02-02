fbb7e7383cedf4d9ac65bd027011be555c6b306a
Bruno Guerra <gguerrabruno@outlook.com>
2022-02-01 23:13:35 -0300

    Option to include uid

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 class Task {
   String? uid;
   String title;
@@ -13,26 +11,15 @@ class Task {
     required this.isDone,
   });
 
-  Task copyWith({
-    String? uid,
-    String? title,
-    String? description,
-    bool? isDone,
-  }) {
-    return Task(
-      uid: uid ?? this.uid,
-      title: title ?? this.title,
-      description: description ?? this.description,
-      isDone: isDone ?? this.isDone,
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
+  Map<String, dynamic> toMap({bool withUid = false}) {
+    final map = {
+      'uid': uid,
       'title': title,
       'description': description,
       'isDone': isDone,
     };
+    if (withUid) map['uid'] = uid;
+    return map;
   }
 
   factory Task.fromMap(Map<String, dynamic> map, {required String id}) {
@@ -47,24 +34,5 @@ class Task {
   @override
   String toString() {
     return 'Task(uid: $uid, title: $title, description: $description, isDone: $isDone)';
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-
-    return other is Task &&
-        other.uid == uid &&
-        other.title == title &&
-        other.description == description &&
-        other.isDone == isDone;
-  }
-
-  @override
-  int get hashCode {
-    return uid.hashCode ^
-        title.hashCode ^
-        description.hashCode ^
-        isDone.hashCode;
   }
 }
